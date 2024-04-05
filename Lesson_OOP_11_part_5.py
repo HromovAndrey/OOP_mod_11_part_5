688dac33904227f64808dda42f0b46938b6328cf
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-04-05 21:42:38 +0300

    Приклад

--- Lesson_OOP_11_part_5.py
+++ Lesson_OOP_11_part_5.py
@@ -59,22 +59,69 @@
 # cat.get_super()
 # cat.sound()
 
-class Figure:
-    def __init__(self, area):
-        self._area = area
-        print("init from Figure")
+# class Figure:
+#     def __init__(self, area):
+#         self._area = area
+#         print("init from Figure")
+#
+# class Circle(Figure):
+#     def __init__(self, area, radius):
+#         super().__init__(area)
+#         self._radius = radius
+#         print("init from Circle")
+#
+#
+#     def print_info(self):
+#         print(f"{self._area=}")
+#         print(f"{self._radius=}")
+#
+# obj = Circle(20, 3)
+# obj.print_info()
+#
+# class Person:
+#     def __init__(self, name, surname, place_of_birth, year_of_birth):
+#         self.name = name
+#         self.surname = surname
+#         self.place_of_birth = place_of_birth
+#         self.year_of_birth = year_of_birth
+#
+#     def print_info(self, n):
+#         for i in range(n):
+#             print(f"name: {self.name}, surname: {self.surname}, Place of birth:{self.place_of_birth}, year_of_birth:{self.year_of_birth}")
+#
+#
+#     def get_age(self, current_yaer):
+#         return current_yaer - self.year_of_birth
+#
+# p1 = Person("Elon", "Musk", "UAR", 1971)
+# p2 = Person("Sergii", "Korolev", "Russia", 1907)
+# p3 = Person("Albert", "Einstein", "Germany", 1879)
+#
+# p1.print_info(1)
+# p2.print_info(1)
+# p3.print_info(1)
 
-class Circle(Figure):
-    def __init__(self, area, radius):
-        super().__init__(area)
-        self._radius = radius
-        print("init from Circle")
+
+# class Cat:
+#     breed = "pars"
+#
+#     def set_value(self, value, age=0):
+#         self.name = name
+#         self.age = age
+#
+#     def __init__(self):
+#        print("Hello new object", self)
+#
+# Jerry = Cat
+
+class Person:
+     age = 18
+     def __init__(self, x, y):
+         self.height = x
+         self.age = y
+p = Person(179, "Andrii")
+print(p.__dict__)
 
 
-    def print_info(self):
-        print(f"{self._area=}")
-        print(f"{self._radius=}")
 
-obj = Circle(20, 3)
-obj.print_info()
 
