2ffba41e3087ba55804340722ac0cb398fe8a1d1
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-04-05 21:36:53 +0300

    Урок 5 практика завдання 1

--- OOP_mod_11_part_5.py
+++ OOP_mod_11_part_5.py
@@ -1,2 +1,45 @@
-class Dekor
-    отроир
+#Завдання 1
+#Створіть клас Human, який міститиме інформацію
+#про людину.
+#За допомогою механізму успадкування реалізуйте
+#клас Builder (містить інформацію про будівельника),
+#клас Sailor (містить інформацію про моряка), клас Pilot
+#(містить інформацію про льотчика).
+#Кожен із класів має містити необхідні для роботи
+# #методи.
+#
+#
+# class Human:
+#     def sound(self):
+#         print("Some sound: go to work")
+# class Builder(Human):
+#     def sound(self):
+#         print("working")
+#     def run(self):
+#         print("builds")
+# class Sailor(Human):
+#     def swiming(self):
+#         print("swiming")
+#
+#     def sound(self):
+#         print("Working")
+#
+# class Pilot(Human):
+#     def fly(self):
+#          print("Fly")
+#
+# obj = Builder()
+# obj.sound()
+# obj.run()
+#
+# obj = Sailor()
+# obj.sound()
+# obj.swiming()
+#
+# obj = Pilot()
+# obj.sound()
+
+
+
+
+
