3cf50527faa8da2889b7170d8bafd43f2acd9f83
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-04-05 21:38:18 +0300

    Урок 5 практика завдання 2

--- OOP_mod_11_part_5.py
+++ OOP_mod_11_part_5.py
@@ -1,45 +1,40 @@
-#Завдання 1
-#Створіть клас Human, який міститиме інформацію
-#про людину.
-#За допомогою механізму успадкування реалізуйте
-#клас Builder (містить інформацію про будівельника),
-#клас Sailor (містить інформацію про моряка), клас Pilot
-#(містить інформацію про льотчика).
-#Кожен із класів має містити необхідні для роботи
-# #методи.
-#
-#
-# class Human:
-#     def sound(self):
-#         print("Some sound: go to work")
-# class Builder(Human):
-#     def sound(self):
-#         print("working")
-#     def run(self):
-#         print("builds")
-# class Sailor(Human):
-#     def swiming(self):
-#         print("swiming")
-#
-#     def sound(self):
-#         print("Working")
-#
-# class Pilot(Human):
-#     def fly(self):
-#          print("Fly")
-#
-# obj = Builder()
-# obj.sound()
-# obj.run()
-#
-# obj = Sailor()
-# obj.sound()
-# obj.swiming()
-#
-# obj = Pilot()
-# obj.sound()
+# Завдання 2
+# Створіть клас Passport (паспорт), який міститиме
+# паспортну інформацію про громадянина заданої країни.
+# За допомогою механізму успадкування реалізуйте
+# клас ForeignPassport (закордонний паспорт), похідний
+# від Passport.
+# Нагадаємо, що закордонний паспорт містить, крім
+# паспортних даних, дані про візи і номер закордонного
+# паспорта.
+# Кожен із класів має містити необхідні методи.
+class Passport:
+    def __init__(self, name, passport_number, age, country):
+        self.name = name
+        self.passport_number = passport_number
+        self.age = age
+        self.country = country
 
 
+class ForeignPassport(Passport):
+    def __init__(self, name, passport_number, age, country, visas):
+        super().__init__(name, passport_number, age, country)
+        self.passport_number = passport_number
+        self._visas = visas
+
+    def show_info(self):
+        super().show_info()
+        print(f"Foreign Passport Number: {self.foreign_passport_number}")
+        print("Visa Information:")
+        for visa in self.visa_info:
+            print(f"- {visa}")
+
+
+passport = Passport("John Doe", "AB123456", "USA")
+passport.show_info()
+print()
+foreign_passport = ForeignPassport("Alice Smith", "CD789012", "UK", ["USA", "Canada"], "XYZ987654")
+foreign_passport.show_info()
 
 
 
